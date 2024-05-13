2c48871eccdca4f2bf9b8407e6a4673ab57a6216
Lixing Xiao <111164872+tryhiseyyysum@users.noreply.github.com>
2024-05-13 21:08:01 +0800

    Add files via upload

--- run.py
+++ run.py
@@ -1,40 +1,66 @@
 from lib.config import cfg, args
 import numpy as np
 import os
+from training_loop import TrainLoop
+from lib.networks.make_network import create_gaussian_diffusion
+from lib.utils.parser_util import *
 ### SCRIPTS BEGINING ###
 def run_dataset():
-    from lib.datasets import make_data_loader
+    from lib.datasets.make_dataset import make_data_loader
+    from lib.utils.data_utils import to_cuda
     import tqdm
 
     cfg.train.num_workers = 0
-    data_loader = make_data_loader(cfg, is_train=False)
+    data_loader = make_data_loader(cfg)
+    total_time = 0
+    import time
     for batch in tqdm.tqdm(data_loader):
-        pass
+        start = time.time()
+        #batch = to_cuda(batch)
+        total_time += time.time() - start
+    print(total_time / len(data_loader))
 
 def run_network():
     from lib.networks import make_network
     from lib.datasets import make_data_loader
     from lib.utils.net_utils import load_network
     from lib.utils.data_utils import to_cuda
+    from lib.utils.fixseed import fixseed
+    from lib.train.train_platforms import ClearmlPlatform, TensorboardPlatform, NoPlatform  # required for the eval operation
+
     import tqdm
     import torch
     import time
 
-    network = make_network(cfg).cuda()
-    load_network(network, cfg.trained_model_dir, epoch=cfg.test.epoch)
+    args = train_args()
+    fixseed(args.seed)
+    network = make_network(cfg)
+    diffusion = create_gaussian_diffusion(args)
+    
+    #load_network(network, cfg.trained_model_dir, epoch=cfg.test.epoch)
+
+    train_platform_type = eval(args.train_platform_type)
+    train_platform = train_platform_type(args.save_dir)
+    train_platform.report_args(args, name='Args')
+
+    data_loader = make_data_loader(cfg)
+
+    TrainLoop(args, train_platform, network, diffusion, data_loader).run_loop()
+
     network.eval()
 
-    data_loader = make_data_loader(cfg, is_train=False)
-    total_time = 0
-    for batch in tqdm.tqdm(data_loader):
-        batch = to_cuda(batch)
-        with torch.no_grad():
-            torch.cuda.synchronize()
-            start = time.time()
-            network(batch)
-            torch.cuda.synchronize()
-            total_time += time.time() - start
-    print(total_time / len(data_loader))
+
+    
+    # total_time = 0
+    # for batch in tqdm.tqdm(data_loader):
+    #     batch = to_cuda(batch)
+    #     with torch.no_grad():
+    #         torch.cuda.synchronize()
+    #         start = time.time()
+    #         network(x=batch,timesteps=32)
+    #         torch.cuda.synchronize()
+    #         total_time += time.time() - start
+    # print(total_time / len(data_loader))
 
 
 def run_evaluate():
@@ -46,29 +72,33 @@ def run_evaluate():
     from lib.utils import net_utils
     import time
 
-    network = make_network(cfg).cuda()
+    network = make_network(cfg)
+    
     net_utils.load_network(network,
                            cfg.trained_model_dir,
                            resume=cfg.resume,
                            epoch=cfg.test.epoch)
     network.eval()
 
-    data_loader = make_data_loader(cfg, is_train=False)
+    data_loader = make_data_loader(cfg)
     evaluator = make_evaluator(cfg)
     net_time = []
     for batch in tqdm.tqdm(data_loader):
-        for k in batch:
-            if k != 'meta':
-                batch[k] = batch[k].cuda()
+        # for k in batch:
+        #     if k != 'meta':
+        #         batch[k] = batch[k].cuda()
         with torch.no_grad():
-            torch.cuda.synchronize()
+            #torch.cuda.synchronize()
             start_time = time.time()
-            output = network(batch)
-            torch.cuda.synchronize()
+            #output = network(batch)
+            #torch.cuda.synchronize()
             end_time = time.time()
         net_time.append(end_time - start_time)
-        evaluator.evaluate(output, batch)
-    evaluator.summarize()
+        #evaluator.evaluate(output, batch)
+    #evaluator.summarize()
+
+    
+
     if len(net_time) > 1:
         print('net_time: ', np.mean(net_time[1:]))
         print('fps: ', 1./np.mean(net_time[1:]))
@@ -86,7 +116,7 @@ def run_visualize():
     from lib.visualizers import make_visualizer
     from lib.utils.data_utils import to_cuda
 
-    network = make_network(cfg).cuda()
+    network = make_network(cfg)
     load_network(network,
                  cfg.trained_model_dir,
                  resume=cfg.resume,
@@ -96,7 +126,7 @@ def run_visualize():
     data_loader = make_data_loader(cfg, is_train=False)
     visualizer = make_visualizer(cfg)
     for batch in tqdm.tqdm(data_loader):
-        batch = to_cuda(batch)
+        #batch = to_cuda(batch)
         with torch.no_grad():
             output = network(batch)
         visualizer.visualize(output, batch)
--- train_net.py
+++ train_net.py
@@ -1,7 +1,8 @@
 from lib.config import cfg, args
 from lib.networks import make_network
 from lib.train import make_trainer, make_optimizer, make_lr_scheduler, make_recorder, set_lr_scheduler
-from lib.datasets import make_data_loader
+from lib.datasets import make_dataset
+from lib.datasets.make_dataset import make_data_loader
 from lib.utils.net_utils import load_model, save_model, load_network, save_trained_config, load_pretrain
 from lib.evaluators import make_evaluator
 import torch.multiprocessing
@@ -17,11 +18,8 @@ if cfg.fix_random:
 
 
 def train(cfg, network):
-    train_loader = make_data_loader(cfg,
-                                    is_train=True,
-                                    is_distributed=cfg.distributed,
-                                    max_iter=cfg.ep_iter)
-    val_loader = make_data_loader(cfg, is_train=False)
+    train_loader = make_data_loader(cfg)
+    val_loader = make_data_loader(cfg)
     trainer = make_trainer(cfg, network, train_loader)
 
     optimizer = make_optimizer(cfg, network)
