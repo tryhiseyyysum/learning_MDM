66e0bdea783d734d09b9ef9f4c71d8e0e9e864de
Lixing Xiao <111164872+tryhiseyyysum@users.noreply.github.com>
2024-05-13 21:12:06 +0800

    Add files via upload

---
+++ lib/networks/networks/make_network.py
@@ -0,0 +1,89 @@
+import os
+import imp
+from diffusion import gaussian_diffusion as gd
+from diffusion.respace import SpacedDiffusion, space_timesteps
+from lib.utils.parser_util import get_cond_mode
+
+def make_network(cfg):
+    # module = cfg.network_module
+    # path = cfg.network_path
+    module = 'lib.networks.mdm.network'
+    path = 'lib/networks/mdm/network.py'
+    # network = imp.load_source(module, path).Network()
+    network = imp.load_source(module, path).Network(**get_model_args(cfg.args2, cfg.data))
+    print(network)
+    return network
+
+def get_model_args(args, data):
+
+    # default args
+    clip_version = 'ViT-B/32'
+    action_emb = 'tensor'
+    cond_mode = 'text'
+    if hasattr(data.dataset, 'num_actions'):
+        num_actions = data.dataset.num_actions
+    else:
+        num_actions = 1
+
+    # SMPL defaults
+    data_rep = 'rot6d'
+    njoints = 25
+    nfeats = 6
+
+    if args.dataset == 'humanml3d':
+        data_rep = 'hml_vec'
+        njoints = 263
+        nfeats = 1
+    elif args.dataset == 'kit':
+        data_rep = 'hml_vec'
+        njoints = 251
+        nfeats = 1
+
+    return {'modeltype': '', 'njoints': njoints, 'nfeats': nfeats, 'num_actions': num_actions,
+            'translation': True, 'pose_rep': 'rot6d', 'glob': True, 'glob_rot': True,
+            'latent_dim': args.latent_dim, 'ff_size': 1024, 'num_layers': args.layers, 'num_heads': 4,
+            'dropout': 0.1, 'activation': "gelu", 'data_rep': data_rep, 'cond_mode': cond_mode,
+            'cond_mask_prob': args.cond_mask_prob, 'action_emb': action_emb, 'arch': args.arch,
+            'emb_trans_dec': args.emb_trans_dec, 'clip_version': clip_version, 'dataset': args.dataset}
+
+
+def create_gaussian_diffusion(args):
+    # default params
+    predict_xstart = True  # we always predict x_start (a.k.a. x0), that's our deal!
+    steps = args.diffusion_steps
+    #steps = 20
+    scale_beta = 1.  # no scaling
+    timestep_respacing = ''  # can be used for ddim sampling, we don't use it.
+    learn_sigma = False
+    rescale_timesteps = False
+
+    #betas = gd.get_named_beta_schedule(args.noise_schedule, steps, scale_beta)
+    betas = gd.get_named_beta_schedule('linear',steps)
+
+    loss_type = gd.LossType.MSE
+
+    if not timestep_respacing:
+        timestep_respacing = [steps]
+
+    return SpacedDiffusion(
+        use_timesteps=space_timesteps(steps, timestep_respacing),
+        betas=betas,
+        model_mean_type=(
+            gd.ModelMeanType.EPSILON if not predict_xstart else gd.ModelMeanType.START_X
+        ),
+        model_var_type=(
+            (
+                gd.ModelVarType.FIXED_LARGE
+                # if not args.sigma_small
+                # else gd.ModelVarType.FIXED_SMALL
+                
+            )
+            if not learn_sigma
+            else gd.ModelVarType.LEARNED_RANGE
+        ),
+        loss_type=loss_type,
+        rescale_timesteps=rescale_timesteps,
+        lambda_vel=args.lambda_vel,
+        lambda_rcxyz=args.lambda_rcxyz,
+        lambda_fc=args.lambda_fc,
+    )
